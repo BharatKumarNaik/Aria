1bf9f7806fa753e602c510bd5174d2a38f8c2750
BharatKumarNaik <88843733+BharatKumarNaik@users.noreply.github.com>
2022-07-15 03:27:55 +0530

    Delete aria directory

--- aria/aria1/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from aria1.models import Contact
-
-# Register your models here.
-admin.site.register(Contact)
--- aria/aria1/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class Aria1Config(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'aria1'
